82f3cf91087dc1e18c0f88ead09b1a379c377004
poststack <dundukpost@gmail.com>
2023-10-15 09:34:27 +0300

    Initial commit

---
+++ Assets/CameraAnimation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraAnimation : MonoBehaviour
+{
+	public GameObject camera2;
+	public GameObject camera3;
+	public GameObject camera4main;
+	public GameObject ceilingObject;
+	[Space(20)]
+
+	public float tile2;
+	public float tile3;
+	public float tile4;
+
+
+	protected void Start()
+	{
+		camera2.SetActive(false);
+		camera3.SetActive(false);
+		camera4main.SetActive(false);
+		ceilingObject.SetActive(true);
+
+	}
+
+	private void Update()
+	{
+		if (Time.time >= tile2)
+		{
+			camera2.SetActive(true);
+		}
+
+		if (Time.time >= tile3)
+		{
+			camera3.SetActive(true);
+		}
+
+		if (Time.time >= tile4)
+		{
+			camera4main.SetActive(true);
+			ceilingObject.SetActive(false);
+			StartCoroutine(SwitchOffAfterDelay());
+	
+		}
+		
+	IEnumerator SwitchOffAfterDelay()
+	{
+		yield return new WaitForSeconds(0.1f);
+		gameObject.SetActive(false);
+	}
+
+	}
+}
---
+++ Assets/CatFactRequest.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class CatFactRequest : MonoBehaviour
+{
+	private const string Url = "https://catfact.ninja/fact";
+	private const string AcceptHeader = "application/json";
+	private const string CsrfTokenHeader = "VP2CPsLUhUCaPNMhxLmLmySrIcA6QBbNwICRtSYT";
+	
+	
+	
+	public Text​Mesh​Pro​UGUI textObject;
+	public string initialText;
+	
+	protected void Start()
+	{
+		initialText = textObject.text;
+	}
+
+	public void GetRandomFact()
+	{
+		StartCoroutine(GetCatFact());
+	}
+
+	private IEnumerator GetCatFact()
+	{
+		UnityWebRequest request = UnityWebRequest.Get(Url);
+		request.SetRequestHeader("accept", AcceptHeader);
+		request.SetRequestHeader("X-CSRF-TOKEN", CsrfTokenHeader);
+
+		yield return request.SendWebRequest();
+
+		if (request.result == UnityWebRequest.Result.Success)
+		{
+			string responseJson = request.downloadHandler.text;
+			// Parse the JSON response
+			CatFact catFact = JsonUtility.FromJson<CatFact>(responseJson);
+
+			Debug.Log($"Cat Fact: {catFact.fact}");
+			
+			textObject.text = initialText + catFact.fact;
+		}
+		else
+		{
+			Debug.LogError($"Request failed. Error: {request.error}");
+		}
+	}
+}
+
+[System.Serializable]
+public class CatFact
+{
+	public string fact;
+	public int length;
+}
